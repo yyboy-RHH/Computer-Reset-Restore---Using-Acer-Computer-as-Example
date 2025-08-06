9b8e69a1102203b5116f0a6f0d9ff4ea9093c903
RONG HSUAN, HSU aka  Robert, Hsu <73116334+yyboy-RHH@users.noreply.github.com>
2025-08-06 14:43:03 +0800

    Add files via upload
    upload

---
+++ Windows Command Prompt command.py
@@ -0,0 +1,27 @@
+# entering the windows via the terminal:
+
+'''
+
+
+shutdown /r /o /t 0 – What is this?
+
+This is a Windows Command Prompt command used to restart the computer and enter Advanced Startup Options.
+
+Command breakdown:
+
+    shutdown – Windows shutdown/restart command
+    /r – Restart
+    /o – Restart and go to the Advanced Startup Options menu
+    /t 0 – Time delay 0 seconds (execute immediately)
+
+How to use it:
+
+    Press Win + R to open the Run window
+    Type cmd to open Command Prompt
+    Enter shutdown /r /o /t 0 and press Enter
+    The computer will immediately restart and show the blue Choose an option screen you saw earlier
+
+So, shutdown /r /o /t 0 is a quick command to restart immediately and enter Advanced Startup Options, faster than doing it manually.
+
+
+'''
